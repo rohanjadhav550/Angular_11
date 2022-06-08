02ffa43616f959a05438a6b4685ae182fb420019
rohanjdhv550 <rohanjdhv97@gmail.com>
2022-06-08 12:29:01 +0530

    Understood how to use structural directives such as ng-template, ng-if and ng-for

---
+++ ng-template-ng-if-ng-for-Demo/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  templateFlag = false;
+  images = [
+    'https://picsum.photos/id/1/300/300',
+    'https://picsum.photos/id/2/300/300',
+    'https://picsum.photos/id/3/300/300'
+  ]
+
+  dispTemplate(){
+    this.templateFlag = !this.templateFlag;
+  }
+}
